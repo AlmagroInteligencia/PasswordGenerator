38a5b26ac81deb36ec0b2844af895578b4889de6
Marcos Pyznar <pyznar.marcos@gmail.com>
2021-11-07 23:40:17 -0300

    terminado

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+import random
+
 def home(request):
     context={  
     }
@@ -10,4 +12,24 @@ def about(request):
     }
     return render(request, 'generator/about.html', context)
 
+def password(request):
+    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    password_generated = ''
+
+    pass_lenght = int(request.GET.get('length'))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+    if request.GET.get('special_chars'):
+        characters.extend(list('-_!#$%&/()=?¡'))
+
+    for x in range(pass_lenght):
+        password_generated += random.choice(characters)
+
+    context={
+        'password' : password_generated
+    }
+    return render(request, 'generator/password.html', context)
 
